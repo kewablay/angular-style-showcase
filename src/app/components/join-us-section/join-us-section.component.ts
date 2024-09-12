8de8ba256c6bdd0637b39c8978b07a6a3b4d78c8
kewablay <kojokewajunior123@gmail.com>
2024-09-12 16:31:13 +0000

    added join us section

---
+++ src/app/components/join-us-section/join-us-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-us-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './join-us-section.component.html',
+  styleUrl: './join-us-section.component.sass'
+})
+export class JoinUsSectionComponent {
+
+}
